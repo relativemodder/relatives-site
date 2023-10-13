74b5ee53732794efa76f0ae2e51c3d32357ff4bc
Ivan <kezin636@gmail.com>
2023-10-13 14:36:14 +0300

    meta tags

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,12 @@
 <script>
+    import { assets } from '$app/paths';
     import '../css/styles.css';
 </script>
+
+<svelte:head>
+    <meta name="og:title" content="Relative's personal website" />
+    <meta name="og:description" content="My bio" />
+    <meta name="og:image" content={assets + "/my-icon.png"} />
+</svelte:head>
 
 <slot />
